54bb9b5d194988fd478d6f12f338635cda56280b
saasbook <saasbook@saasbook.(none)>
2013-04-09 14:13:47 -0700

    Initialisation RoR

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CompMS::Application.config.secret_token = '1558f83cbfaf3c5a8961f0c25d83e578a08b164a49bed1c597e0b74d89acf1ece1b420dd6384cf4a42435d56dad3feb48f820d211911105b3a992a4df77b5408'
